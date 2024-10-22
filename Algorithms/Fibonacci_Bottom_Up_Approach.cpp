41f290f679892bf93620f401a285444a948f0fe0
code-epic-adi <aditya308989@gmail.com>
2024-10-22 23:19:06 +0530

    N-th Fibonacci number using Bottom Up Approch of DP

---
+++ Algorithms/Fibonacci_Bottom_Up_Approach.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+ll fib(int n, vector<ll>& arr) {
+
+    if (arr[n] != -1) {
+        return arr[n];
+    }
+
+    ll f;
+    if (n <= 2) {
+        f = 1;
+    } else {
+        f = fib(n - 1, arr) + fib(n - 2, arr);
+    }
+
+    arr[n] = f; // Store the result in the memoization array
+    return f;
+}
+
+int main() {
+    // Code to Find n-th Fibonacci in Linear Running Time
+    int n;
+    cin >> n;
+    vector<ll> vis(n + 1, -1);
+
+    cout << n << "-th Fibonacci Number is " << fib(n, vis) << endl;
+
+    /* Time Complexity: O(n)
+       Space Complexity: O(n) for Visited Array + O(n) for Recursion Stack */
+}
