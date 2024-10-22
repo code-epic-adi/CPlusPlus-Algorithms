76c0bac854eec80c9b7bca37855eaba4511cdf0a
code-epic-adi <aditya308989@gmail.com>
2024-10-22 20:53:36 +0530

    Fibonacci_DP.cpp

---
+++ Algorithms/Fibonacci_DP.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+ll fib(int n, vector<ll>& arr) {
+    if (arr[n] != -1) {
+        return arr[n];
+    }
+
+    ll f;
+    if (n <= 2) {
+        f = 1;
+    } else {
+        f = fib(n - 1, arr) + fib(n - 2, arr);
+    }
+
+    arr[n] = f;
+
+    return f;
+}
+
+int main() {
+    // Code to Find n-th Fibonacci in Linear Running Time
+    int n;
+    cin >> n;
+
+    vector<ll> vis(n + 1, -1); // Use vector for dynamic array initialization
+
+    cout << n << "-th Fibonacci Number is " << fib(n, vis) << endl;
+
+    /* Time Complexity: O(n)
+       Space Complexity: O(n) for Visited Array + O(n) for Recursion Stack */
+}
